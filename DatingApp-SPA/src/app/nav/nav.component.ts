ffb8fa626da640765c7b92104a1e8ae35a370887
alain steve <alainstevemuhirwa@yahoo.com>
2020-04-29 11:13:46 +0200

    End of Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -17,8 +17,9 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in Successfully');
-    }, () => {
-      console.log('Failed to Login');
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
     });
   }
 
